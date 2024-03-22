b2ee172eb2e8b5c8343a5ef1f97b9bcb17d6d3e2
savitsharma <sharmasavit92@gamil.com>
2024-03-22 03:21:13 -0700

    first commit

---
+++ src/main/java/com/example/JWTAuthenticationSpringboot/models/JwtResponse.java
@@ -0,0 +1,16 @@
+package com.example.JWTAuthenticationSpringboot.models;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class JwtResponse {
+    private String jwtToken;
+    private String username;
+
+
+}
